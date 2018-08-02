7f6001f3fa8e4f4e3a17f59d384c8bf7d0cd2748
Stuart Hopwood <stoo.hopwood@gmail.com>
2018-08-02 05:01:52 +0100

    Added Todo's and logging entries and method documentation.

---
+++ Shuffle3/Model/CellFormat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Shuffle3.Model
+{
+    public class CellFormat
+    {
+        public string DisplayCharacter { get; set; }
+        public ConsoleColor DisplayColour { get; set; }
+    }
+}
--- Shuffle3/Model/Position.cs
+++ Shuffle3/Model/Position.cs
@@ -14,6 +14,11 @@
         public int X { get; }
         public int Y { get; }
 
+        /// <summary>
+        /// Position Object for referencing a cell on the board.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
         public Position(int row, int col)
         {
             X = row;
