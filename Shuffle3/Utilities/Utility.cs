3224d26cbbffb6aae9c1115b6435179742206559
Stuart Hopwood <stuart.hopwood@interactivets.com>
2018-08-06 16:13:58 +0100

    Added a Todo

--- Shuffle3/Utilities/Utility.cs
+++ Shuffle3/Utilities/Utility.cs
@@ -2,7 +2,7 @@
 {
     public class Utility
     {
-        public bool IsStringTooLong(int maxLength, string String)
+        public bool IsStringTooLong(int maxLength, string String) // Todo - Add Tests (True & False)
         {
             return String.Length > maxLength;
         }
